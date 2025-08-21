71393ed42f472ca117a0b3ef95ed737873472669
Hadi T <hadi@syniol.com>
2025-08-22 00:36:25 +0100

    added example package test

--- example/main.go
+++ example/main.go
@@ -3,5 +3,7 @@ package main
 import "github.com/syniol/xyo-sdk-go"
 
 func main() {
-	xyo.NewClient(&xyo.ClientConfig{APIKey: ""})
+	xyo.NewClient(&xyo.ClientConfig{APIKey: "RandomBase64EncodedStringApiKey"})
+
+	println("Successfully imported and instantiated the XYO Client")
 }
